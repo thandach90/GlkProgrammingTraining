66f3bdd326a0a1f5d88cdb6f0866a8ba69549674
Theo Handach <Theo.Handach@glock.us>
2018-08-06 15:54:53 -0400

    initial commit

---
+++ Problems.Test/UnitTest1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Problems.Test
+{
+    [TestClass]
+    public class BasicProgrammingIsTrouble
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            //arrange
+            bool aYawning = true;
+            bool bYawning = true;
+
+            //act
+            bool isTrouble = BasicProgramming.IsTrouble(aYawning, bYawning);
+
+            //assert
+            Assert.AreEqual(true,isTrouble,"Both cats are yawning so we need to be in trouble which means return true");
+
+
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            bool aYawning = false;
+            bool bYawning = false;
+
+            //act
+            bool isTrouble = BasicProgramming.IsTrouble(aYawning, bYawning);
+
+            //assert
+            Assert.AreEqual(true, isTrouble, "Both cats are not yawning so we need to be in trouble, which means return true.");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            bool aYawning = true;
+            bool bYawning = false;
+
+            //act
+            bool isTrouble = BasicProgramming.IsTrouble(aYawning, bYawning);
+
+            //assert
+            Assert.AreEqual(false, isTrouble, "One cat is yawning so we are not in trouble, which means return false");
+        }
+    }
+
+    [TestClass]
+    public class BasicProgrammingIsHooky
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            //arrange
+            bool weekday = true;
+            bool golf = true;
+
+            //act
+            bool isHooky = BasicProgramming.IsHooky(weekday, golf);
+
+            //assert
+            Assert.AreEqual(true, isHooky, "Its a weekend and we are playing golf so return true.");
+
+
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            bool weekday = false;
+            bool golf = false;
+
+            //act
+            bool isHooky = BasicProgramming.IsHooky(weekday, golf);
+
+            //assert
+            Assert.AreEqual(true, isHooky, "Its a weekend so we are playing hooky which means true.");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            bool weekday = true;
+            bool golf = false;
+
+            //act
+            bool isHooky = BasicProgramming.IsHooky(weekday, golf);
+
+            //assert
+            Assert.AreEqual(false, isHooky, "Its a weekday but we are not playing golf so we return false here");
+        }
+    }
+}
---
+++ Problems/BasicProgramming.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public static class BasicProgramming
+    {
+
+        /// <summary>
+        /// We have two cats, a and b, and the parameters aYawn and aYawn indicate if each is yawning. We are in trouble if they are both yawning or if neither of them is yawning. Return true if we are in trouble.
+        /// </summary>
+        /// <param name="aYawn"></param>
+        /// <param name="bYawn"></param>
+        /// <returns></returns>
+        public static bool IsTrouble(bool aYawn, bool bYawn)
+        {
+            return false; //placeholder
+        }
+
+        /// <summary>
+        /// The parameter weekday is true if its a weekday and false if its on the weekend. The parameter golf is true if we play golf and false if we don't. If its a weekday and we play golf we play hooky, we also play hooky if its not a weekday so return true for these two conditions.
+        /// </summary>
+        /// <param name="weekDay"></param>
+        /// <param name="golf"></param>
+        /// <returns></returns>
+        public static bool IsHooky(bool weekDay, bool golf)
+        {
+            if (!weekDay)
+            {
+                return true;
+            }
+            else if (weekDay && golf)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
