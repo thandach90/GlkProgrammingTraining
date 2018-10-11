57812a826751e001a49445cde215e138e939b0b4
Theo Handach <Theo.Handach@glock.us>
2018-10-11 08:03:20 -0400

    basic problems 3

---
+++ Problems/BasicProgramming3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public static class BasicProgramming3
+    {
+        /// <summary>
+        /// We are given a string str that will be manipulated and a integer num which represents the factor of which charcters we want to pull from the string. For instance if num is 2, we want to return the chars at 0,2,4...num.length. The beginning will always start from zero so count from there and str will never be a empty string
+        /// 
+        /// Example: str = basketball, num =2         Output = bsebl  
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static string EveryOther(string str, int num)
+        {
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// We are given a string str and a integer num. Return a string that uses str to copy itself multiple times bases on what is passed in for num.
+        /// 
+        /// Example: str = 'cat' num = 3        Output: 'catcatcat'
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static string StringTimesItSelf(string str, int num)
+        {
+            return string.Empty;
+        }
+    }
+}
